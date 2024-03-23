71df82b481b62d90884dbbb405c283a4ad0666f4
ahaul <ahadul70@gmail.com>
2024-03-23 22:23:26 +0600

    leanring video calling

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography,AppBar } from "@mui/material"
+
+const App = () => {
+  return (
+    <div>
+      <AppBar position='static' color='inherit'>
+      
+      <Typography variant='h2' align='center'>Video Chat</Typography>
+      </AppBar>
+      {/*VideoPlayer*/}
+      { /*Options */}
+    </div>
+  )
+}
+
+export default App
