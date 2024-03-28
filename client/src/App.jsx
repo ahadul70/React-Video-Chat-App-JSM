54809841a7bf00177f81f0d3037f8c394fd3c493
ahaul <ahadul70@gmail.com>
2024-03-29 00:39:12 +0600

    struggling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { Typography,AppBar } from "@mui/material"
+import React from "react";
+import { Typography, AppBar } from "@mui/material";
+import VideoPlayer from "./components/VideoPlayer";
+import Option from "./components/Option";
+import Notification from "./components/Notification";
+import { makeStyles } from "@mui/styles"; // Import makeStyles from @mui/styles
+
+
 
 const App = () => {
+ // Fix the variable name
   return (
     <div>
-      <AppBar position='static' color='inherit'>
-      
-      <Typography variant='h2' align='center'>Video Chat</Typography>
+      <AppBar  position="static" color="inherit">
+        {" "}
+        {/* Fix the class name */}
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
       </AppBar>
-      {/*VideoPlayer*/}
-      { /*Options */}
+      <VideoPlayer />
+      <Option>
+        <Notification />
+      </Option>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
